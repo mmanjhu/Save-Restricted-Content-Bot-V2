5bcbacb6f6a866df0a09c26523cd70493a0dd1da
mmanjhu <manishmanjhu3333@gmail.com>
2025-01-13 23:34:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+API_ID = int(getenv("API_ID", "8054489757"))
+API_HASH = getenv("API_HASH", "b2526517ed963b1951a811b24bc29a4e")
+BOT_TOKEN = getenv("BOT_TOKEN", "7477970001:AAEG6bSxz6oE4U7YPVJmvAkhbElBPsiBebY")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8054489757").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Mmanjhu:O9UjosQUi9wWkfAd@cluster0.ukzu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
-STRING = getenv("STRING", None)
+AD_API = getenv("AD_API", "28549267"
+STRING = getenv("STRING", BQGzoJMAHbTCJ0O-lTT5R5OBtTUvajW0QghO58TVuOalvGbwL3nTKe_dpeKLj1ENXZW50MZNKTZ8rkB-45um8GKAaNxnKRFK8RKPTbxzDNRoBYBHV_CjeC2NaHBWQ3illg28VBRu1_uWhlrRQbkcBtKH8TUNnvSuedvyAAEiNzjVmW4a9rq5Vc-jOOVsx1s2oX8WEIFCyC4fc18bVxPXrwhxP1MfD_P1FEef6RqncgpmPmNLfNKxcsmiOExz7w97J8w3WDzG8T1errl2c7uxxESdAPISIqD19yp6U_GE0PRyhDvJVq0Rwqas_w4r_ax21sseWQ4Cx8dFkEghj0TAtpNGma3tigAAAAHgFcKdAA 
 YT_COOKIES = getenv("YT_COOKIES", None)
 INSTA_COOKIES = getenv("INSTA_COOKIES", None)
